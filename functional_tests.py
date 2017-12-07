f607928202b703c080c33d9d5dacf9903411edd3
Gene McDaniel <mcdaniel.gene@gmail.com>
2017-12-07 17:46:42 -0500

    First commit: First FT and basic Django config

---
+++ functional_tests.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+browser = webdriver.Firefox()
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title

d56eb781337e22c0b50a8079d4dcdb03cb64febb
Gene McDaniel <mcdaniel.gene@gmail.com>
2017-12-09 15:05:11 -0500

    Model for list Items and associated migration

--- functional_tests.py
+++ functional_tests.py
@@ -14,8 +14,8 @@ class NewVisitorTest(unittest.TestCase):
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [ r.text for r in rows ])
+        rows = tablefind_elements_by_tag_name('t')
+        self.assertIn(row_text, [r.text for r in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith hs heard about a cool new online to-do app. She goes
@@ -37,7 +37,7 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
+        time.sleep(1)
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
@@ -46,7 +46,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')

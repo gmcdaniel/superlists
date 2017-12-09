dae84a8448a0ce5eece0328cd13f265981355a02
Gene McDaniel <mcdaniel.gene@gmail.com>
2017-12-09 14:13:04 -0500

    Can handle one list item successfully

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,6 @@
 import unittest
+import time
 from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.common.keys import Keys
 
 
@@ -32,23 +32,28 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(5)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            'New to-do item did not appear in table'
-        )
+        self.assertIn('1: Buy peacock feathers', [ r.text for r in rows ])
 
-        # There is still a tes=xt box inviting her to add another item. She
+        # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(5)
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('2: Use peacock feathers to make a fly', [ r.text for r in rows ])
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a uinique URL for her -- there is some
         # explanatory text to this effect.
+        self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
 

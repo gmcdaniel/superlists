e3a0ddcc32a089d36af9a2da18508caa16efeaff
Gene McDaniel <mcdaniel.gene@gmail.com>
2017-12-10 12:03:39 -0500

    Unit tests all working, figuring out one failing FT

--- lists/views.py
+++ lists/views.py
@@ -4,10 +4,17 @@ from lists.models import Item
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the_list/')
 
     items = Item.objects.all()
     #return render(request, 'home.html', {'items': items})
+    todos = [ '{i}: {s}'.format(i=i, s=s.text)
+        for i, s in enumerate(items, 1)]
+    return render(request, 'home.html', {'items': todos})
+
+def view_list(request):
+    print('*'*20, "In view_list")
+    items = Item.objects.all()
     todos = [ '{i}: {s}'.format(i=i, s=s.text)
         for i, s in enumerate(items, 1)]
     return render(request, 'home.html', {'items': todos})

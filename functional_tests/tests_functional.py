702592fda28dd88a8965df28fee223e74421690a
Gene McDaniel <mcdaniel.gene@gmail.com>
2017-12-10 18:20:50 -0500

    Items now belong to a list

--- functional_tests/tests_functional.py
+++ functional_tests/tests_functional.py
@@ -18,6 +18,9 @@ class NewVisitorTest(LiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [r.text for r in rows])
 
+    def wait_for_browser(self):
+        time.sleep(1)
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith hs heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -38,10 +41,10 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.wait_for_browser()
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
-        time.sleep(1)
+        self.wait_for_browser()
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
@@ -50,7 +53,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.wait_for_browser()
 
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
@@ -74,7 +77,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.wait_for_browser()
 
         # Francis gets his own unique URL.
         francis_list_url = self.browser.current_url
--- lists/views.py
+++ lists/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the_list/')
+    return render(request, 'home.html')
 
-    items = Item.objects.all()
-    #return render(request, 'home.html', {'items': items})
-    todos = [ '{i}: {s}'.format(i=i, s=s.text)
-        for i, s in enumerate(items, 1)]
-    return render(request, 'home.html', {'items': todos})
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/the_list/')
 
 def view_list(request):
     items = Item.objects.all()

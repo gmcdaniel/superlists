144a590c868321eea2e54fef3b993bdf73ba0498
Gene McDaniel <mcdaniel.gene@gmail.com>
2017-12-09 22:48:40 -0500

    Can persist items in db

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from lists.models import Item
 
 def home_page(request):
-    return render(request,
-                  'home.html',
-                  {'new_item_text': request.POST.get('item_text', '')})
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+    #return render(request, 'home.html', {'items': items})
+    todos = [ '{i}: {s}'.format(i=i, s=s.text)
+        for i, s in enumerate(items, 1)]
+    return render(request, 'home.html', {'items': todos})
